0ed3043341e984444fa0f7ed2fc235b3899b8bb6
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-06-15 18:52:09 -0600

    adjust styles

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/app.module.css'
 
 function App (): ReactElement {
     return (
-        <main className={styles.textView}>
+        <main className={styles.center}>
             <p>hi</p>
         </main>
     )
