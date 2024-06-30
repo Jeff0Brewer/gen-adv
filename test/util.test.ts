a3e3c445491f00b1c1b950c993670351de9ef5a0
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-06-29 20:15:28 -0600

    add tests for ensureArray

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,5 +1,8 @@
 import { expect, test } from 'vitest'
-import { randomChoice } from '../src/lib/util'
+import {
+    ensureArray,
+    randomChoice
+} from '../src/lib/util'
 
 test('randomChoice returns element from list of strings', () => {
     const list = ['a', 'b', 'c', 'd', 'e']
@@ -13,4 +16,17 @@ test('randomChoice returns element from list of mixed types', () => {
 
 test('randomChoice returns undefined on empty list', () => {
     expect(randomChoice([])).toBeUndefined()
+})
+
+test('ensureArray returns array containing passed string', () => {
+    expect(ensureArray('string')).toEqual(['string'])
+})
+
+test('ensureArray returns same reference if passed array', () => {
+    const array = ['some', 12, true, {}]
+    expect(ensureArray(array)).toBe(array)
+})
+
+test('ensureArray returns array of null if passed null', () => {
+    expect(ensureArray(null)).toEqual([null])
 })
