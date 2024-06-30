21ead4a9386dcf4295282c206d6fc54f23138ba6
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2024-06-29 21:00:07 -0600

    add transition to story display

--- src/components/story-display.tsx
+++ src/components/story-display.tsx
@@ -6,7 +6,7 @@ function StoryDisplay(): ReactElement {
     const { story } = useGameContext()
 
     return (
-        <section className={styles.story}>
+        <section className={`${styles.story} ${!story && 'hidden'}`}>
             {story.split('\n').map((line, i) => <p key={i}>{line}</p>)}
         </section>
     )
