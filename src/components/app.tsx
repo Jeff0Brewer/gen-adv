16384182e656add4a0a6f7d9758e050525c02da7
Jeff Brewer <jeff@jeMB.local>
2024-10-14 15:32:25 -0600

    delete

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,19 +1,9 @@
 import type { ReactElement } from 'react'
-import GameProvider from '../components/game-provider'
-import InfoDisplay from '../components/info-display'
-import StoryDisplay from '../components/story-display'
-import UserInput from '../components/user-input'
-import styles from '../styles/app.module.css'
+import _styles from '../styles/app.module.css'
 
 function App(): ReactElement {
     return (
-        <GameProvider>
-            <main className={styles.app}>
-                <InfoDisplay />
-                <StoryDisplay />
-                <UserInput />
-            </main>
-        </GameProvider>
+        <></>
     )
 }
 
