c5176e9bcaaddee1079f3c442bd80c091d6cdcf0
Jeff Brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2024-10-16 01:29:59 -0600

    simplify completion

--- src/pages/api/completion.ts
+++ src/pages/api/completion.ts
@@ -5,19 +5,15 @@ const ai = new OpenAI({
     apiKey: process.env.OPENAI_KEY
 })
 
-async function getCompletion(
-    req: NextApiRequest,
-    res: NextApiResponse
-): Promise<void> {
+async function getCompletion(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     const completion = await ai.chat.completions.create({
         model: 'gpt-3.5-turbo',
-        // ChatCompletionMessageParam type from openai is private. I want to die.
+        // TODO: fix type
         /* eslint-disable-next-line */
         messages: req.body.messages
     })
 
-    const message = completion.choices[0].message
-    res.status(200).json(message)
+    res.status(200).json(completion)
 }
 
 export default getCompletion
